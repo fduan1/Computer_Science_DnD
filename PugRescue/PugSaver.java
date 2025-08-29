84c8a1bdbc74d2bce730da81c7aaae0692a0c460
fduan1 <fduan1@hwemail.com>
2025-08-29 11:17:40 -0700

    8/29 saves (myArrayList class)

--- PugRescue/PugSaver.java
+++ PugRescue/PugSaver.java
@@ -1,21 +1,21 @@
-import java.util.ArrayList;
-import java.util.Objects;
+// import java.util.Objects;
 
 public class PugSaver {
 
 	// Moves every dog whose breed is "Pug" in the list to the back of the list
-	public static void rescuePugs(ArrayList<Dog> list) {
+	public static void rescuePugs(MyArrayList<Dog> list) {
 		for (int i = 0; i < list.size(); i++) {
 			if (list.get(i).getBreed().toLowerCase().contains("golden")) {
 				int tempDogPosition = list.size() - 1;
 				for (int j = tempDogPosition; j >= 0; j--) {
 					if (!list.get(j).getBreed().toLowerCase().contains("golden")) {
 						tempDogPosition = j;
-					}
-					if (tempDogPosition < i) {
-						i = list.size();
 						break;
 					}
+				}
+				if (tempDogPosition < i) {
+						i = list.size();
+						break;
 				}
 				Dog tempGolden = list.get(i);
 				list.set(i, list.get(tempDogPosition));
--- PugRescue/PugSaverTester.java
+++ PugRescue/PugSaverTester.java
@@ -1,7 +1,26 @@
+
 public class PugSaverTester {
 
     public static void main(String[] args) {
-        
+        MyArrayList<Dog> dogs = new MyArrayList<Dog>();
+        dogs.add(new Dog("cat", "pug"));
+        dogs.add(new Dog("henry", "golden retriver"));
+        dogs.add(new Dog("bob", "pug"));
+        dogs.add(new Dog("sam", "shitzu"));
+        dogs.add(new Dog("buddy", "terrier"));
+        dogs.add(new Dog("lacy", "daschund"));
+        dogs.add(new Dog("lassie", "poodle"));
+        dogs.add(new Dog("goldie", "golden doodle"));
+        dogs.add(new Dog("gary", "golden doodle"));
+        dogs.add(new Dog("billy", "pug"));
+        dogs.add(new Dog("george", "pug"));
+        dogs.add(new Dog("bart", "pug"));
+        dogs.add(new Dog("lexie", "pug"));
+        dogs.add(new Dog("jess", "golden doodle"));
+
+
+        PugSaver.rescuePugs(dogs);
+        System.out.println(dogs);
     }
 
 }
