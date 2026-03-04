61e722c41276125cc856fd035a36b2be3f9ca1ab
fduan1 <fduan1@hwemail.com>
2026-03-04 13:31:55 -0800

    doen????

---
+++ Markov-2/MarkovPrediction.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MarkovPrediction {
+
+    // Please define your own variables and data structures
+    //
+    public HashMap<String, ArrayList<String>> readData(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            HashMap<String, ArrayList<String>> weatherHashMap = new HashMap<String, ArrayList<String>>();
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] day = line.split(",");
+                if (!weatherHashMap.containsKey(day[0])) {
+                    weatherHashMap.put(day[0], new ArrayList<String>());
+                }
+                weatherHashMap.get(day[0]).add(day[1]);
+            }
+            return weatherHashMap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // Method to predict the next state given a current state
+    public String predictNextState(String currentState) {
+        ArrayList<String> probabilities = readData("weather.csv").get(currentState);
+        int randomNum = (int) (Math.random() * probabilities.size());
+        return probabilities.get(randomNum);
+    }
+
+}
