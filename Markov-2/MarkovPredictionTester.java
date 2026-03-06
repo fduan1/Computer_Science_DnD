19991961b95f7e6eaee2806719a9ee26e62c128d
fduan1 <fduan1@hwemail.com>
2026-03-06 10:10:48 -0800

    finished markov tester?

---
+++ Markov-2/MarkovPredictionTester.java
@@ -0,0 +1,44 @@
+public class MarkovPredictionTester {
+    public static void main(String[] args) {
+        MarkovPrediction weatherPrediction = new MarkovPrediction();
+        weatherPrediction.readData("weather.csv");
+        System.out.println(weatherPrediction.predictNextState("Cloudy"));
+        System.out.println(weatherPrediction.predictNextState("Cloudy"));
+        System.out.println(weatherPrediction.predictNextState("Cloudy"));
+        System.out.println(weatherPrediction.predictNextState("Cloudy"));
+        System.out.println(weatherPrediction.predictNextState("Cloudy"));
+        System.out.println(weatherPrediction.predictNextState("Cloudy"));
+        System.out.println(weatherPrediction.predictNextState("Partly Cloudy"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+        System.out.println(weatherPrediction.predictNextState("Sunny"));
+
+    }
+}
