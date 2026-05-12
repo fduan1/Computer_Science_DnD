7362bec1dee58e0ba48fdb4a62b00cfe55fd3a6b
fduan1 <fduan1@hwemail.com>
2026-05-12 12:46:38 -0700

    brobrobro wtf

--- HuffmansCompression/HuffmansCompressionTester.java
+++ HuffmansCompression/HuffmansCompressionTester.java
@@ -14,10 +14,15 @@ public class HuffmansCompressionTester {
             // HuffmanEncoder.compress("theiss.txt");
             // HuffmanDecoder.decode("theiss.txt.hc");
 
-            HuffmanEncoder.compress("ABCDEFG.txt");
-            HuffmanDecoder.decode("ABCDEFG.txt.hc");
+            // HuffmanEncoder.compress("ABCDEFG.txt");
+            // HuffmanDecoder.decode("ABCDEFG.txt.hc");
 
-            // HuffmanDecoder.decode("Harvard-Westlake Robotics FRC Team 1148 Sponsor Information 25-26.txt.hc");
+            // HuffmanDecoder.decode("Harvard-Westlake Robotics FRC Team 1148 Sponsor Information
+            // 25-26.txt.hc");
+
+            // HuffmanEncoder.compress("gatsby.txt.hc");
+            HuffmanDecoder.decode("gatsby.txt.hc");
+
 
 
         } catch (IOException e) {
